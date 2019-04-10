24f29c22667eb238b102a9464ad518b6b7815e75
Hannah De Los Reyes <htdelosreyes8@gmail.com>
2019-04-10 19:08:11 +0000

    post routes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const postsRouter = require('./routes/posts');
 
 const app = express();
 
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -40,6 +42,7 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+module.exports = indexRouter;
 
 app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   console.log("Server is listening");
