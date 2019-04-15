9c0905fdb8438ef71f91b1e0a3701bd2990ad7c9
Hannah De Los Reyes <htdelosreyes8@gmail.com>
2019-04-15 11:18:42 -0700

    create models

---
+++ models/review.js
@@ -0,0 +1,17 @@
+/* Review
+- body - string
+- author - object id (ref User)
+*/
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ReviewSchema = new Schema ({
+  body: String,
+  auhtor: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }
+});
+
+module.exports = mongoose.model('Review', ReviewSchema);
