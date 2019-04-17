7ea21f9770dd63ec6067763910a802e105600833
Hannah De Los Reyes <htdelosreyes8@gmail.com>
2019-04-17 16:24:44 -0700

    display posts index

--- controllers/posts.js
+++ controllers/posts.js
@@ -10,5 +10,17 @@ module.exports = {
   // Posts New
   newPost(req, res, next) {
     res.render('posts/new');
+  },
+
+  // Posts Create
+  async createPost(req, res, next) {
+    let post = await Post.create(req.body);
+    res.redirect(`/posts/${post.id}`);
+  },
+
+  // Posts Show
+  async showPost(req, res, next) {
+    let post = await Post.findById(req.params.id);
+    res.render('posts/show', { post });
   }
 }
