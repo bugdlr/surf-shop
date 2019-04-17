8d6f2e87b0378d5d90bc76801ddd510d0bb07c11
Hannah De Los Reyes <htdelosreyes8@gmail.com>
2019-04-17 15:57:08 -0700

    add new post form

--- models/post.js
+++ models/post.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const POSTSchema = new Schema ({
+const PostSchema = new Schema ({
   title: String,
   price: String,
   description: String,
