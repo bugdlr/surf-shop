3d2ac3089a44bcdc1d2146bdca6b8f9c3d51d5db
Hannah De Los Reyes <htdelosreyes8@gmail.com>
2019-04-17 15:40:14 -0700

    refactor /login and /logout routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,5 @@
+const passport = require('passport');
+
 const User = require('../models/user');
 module.exports = {
   async postRegister(req, res, next) {
@@ -8,6 +10,17 @@ module.exports = {
     })
     await User.register(newUser, req.body.password);
     res.redirect('/');
-  }
+  },
 
+  postLogin(req, res, next) {
+    passport.authenticate('local', {
+      successRedirect: '/',
+      failureRedirect: '/login'
+    })(req, res, next);
+  },
+
+  getLogout(req, res, next) {
+      req.logout();
+      res.redirect('/');
+    }
 }
