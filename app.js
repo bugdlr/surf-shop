37bf6b51762edd256f47d7eb40e90f1fa1497dc2
Hannah De Los Reyes <htdelosreyes8@gmail.com>
2019-04-19 14:18:42 -0700

    setup mapbox

--- app.js
+++ app.js
@@ -32,6 +32,9 @@ db.once('open', () => {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// setup public assets directory
+app.use(express.static('public'));
+
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
